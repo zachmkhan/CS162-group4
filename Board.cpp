02f95cafcbf84f5caa3cda6360204381f1c66478
Zach <30414090+zachmkhan@users.noreply.github.com>
2018-05-02 11:52:10 -0700

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -8,39 +8,40 @@ Board::Board()
 
     //Creating the 20x20 dynamic 2d array below
     board = new char * [row];
-    for(int count = 0; count < row; ++count)
+    for(int count = 0; count < row; count++)
     {
         board[count] = new char[col];
     }
 
     //Filling the array below
     //I am filling the array with the char 'X' for debugging purposes
-    for(int count = 0; count < row; ++count)
-    {
-        for(int count2 = 0; count2 < col; ++count2);
-        {
-            board[row][col] = 'X'; //This would normally be the space character
-        }
-    }
+	for(int count = 0; count < row; count++)
+	{
+		for(int count2 = 0; count2 < col; count2++)
+		{
+			board[count][count2] = 'X'; //This should print a space in the future
+		}
+	}
 
 }
 
 //This will print the board
 void Board::printBoard()
 {
-    for(int count = 0; count < row; ++count)
+    for(int count = 0; count < row; count++)
     {
-        for(int count2 = 0; count2 < col; ++count2)
+        for(int count2 = 0; count2 < col; count2++)
         {
-            std::cout << board[row][col] << std::endl;
+            std::cout << board[count][count2] << ' ';
         }
+        std::cout<<std::endl;
     }
 }
 
 //This will delete the dynamic array
 void Board::deleteArray()
 {
-    for(int count = 0; count < row; ++count)
+    for(int count = 0; count < row; count++)
     {
         delete [] board[count];
     }
