e874bab5f15bb16adfcda4aef3c840cffd21a6a1
Zach <30414090+zachmkhan@users.noreply.github.com>
2018-05-02 11:28:55 -0700

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -1,1 +1,25 @@
 #include "Board.hpp"
+
+Board::Board()
+{
+    row = 20; //By default
+    col = 20; //By default
+
+    //Creating the 20x20 dynamic 2d array below
+    board = new char * [row];
+    for(int count = 0; count < row; ++count)
+    {
+        board[count] = new char[col];
+    }
+
+    //Filling the array below
+    //I am filling the array with the char 'X' for debugging purposes
+    for(int count = 0; count < row; ++count)
+    {
+        for(int count2 = 0; count2 < col; ++count2);
+        {
+            board[row][col] = 'X';
+        }
+    }
+
+}
