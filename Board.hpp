167ddec563787cbf3a26351b7aba2911ccc07fb4
Zach <30414090+zachmkhan@users.noreply.github.com>
2018-05-02 12:56:17 -0700

    Update Board.hpp

--- Board.hpp
+++ Board.hpp
@@ -8,10 +8,13 @@ class Board
         char ** board; //This will be used to dynamically allocate the array
         int row; //The number of rows (Y value)
         int col; //The number of cols (X value)
+        int ant; //The number of ants
+        int doodle; //The number of doodlebugs
     public:
         Board(); //Default constructor. Will be set to 20x20 by default
-        //Board(int row, int col); //This will possibly be used for the extra credit
+        //Board(int row, int col, int doodle, int ant); //This will possibly be used for the extra credit
         void printBoard(); //Prints the game board
         void deleteArray(); //Frees the dynamic memory
+        void placeRandom(); //Randomly Places ants and doodle bugs
 };
 #endif
