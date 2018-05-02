83351851cd21c1cf59e1c0e21c3f6854485089dc
Zach <30414090+zachmkhan@users.noreply.github.com>
2018-05-02 12:55:36 -0700

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -5,6 +5,8 @@ Board::Board()
 {
     row = 20; //By default
     col = 20; //By default
+    ant = 100; //Default number of ants
+    doodle = 5; //Default number of doodles
 
     //Creating the 20x20 dynamic 2d array below
     board = new char * [row];
@@ -62,4 +64,79 @@ void Board::deleteArray()
         delete [] board[count];
     }
     delete [] board;
+}
+
+//This will randomly place the ants and doodlebugs
+//For now it will not be random
+//I will add this near the end so we know that it works
+void Board::placeRandom()
+{
+
+    //These two for loops will check to make sure that the spaces are not occupied for the doodlebugs
+	for(int count = 0; count < row; count++)
+	{
+		for(int count2 = 0; count2 < col; count2++)
+		{
+            bool doodleFlag = true; //This will be used if a space is occupied;
+            //Adding the doodle bugs to the array
+            while(doodleFlag)
+            {
+                if(doodle == 0) //if there are no doodlebugs
+                {
+                    goto end;
+                }
+
+                //If the space can be occupied by a critter
+    			if(board[count][count2] != 'O' || board[count][count2] != 'X')
+                {
+                    doodleFlag = false;
+                    board[count][count2] = 'X'; //setting the space as a doodlebug
+
+                }
+                else //The space is occupied
+                {
+                    //I will need to call the class to get a new random location
+                }
+            }
+		}
+	}
+
+    end:
+    std::cout << "All of the doodlebugs have been placed" << std::endl;
+
+
+
+     //These two for loops will check to make sure that the spaces are not occupied for the ants
+	for(int count = 0; count < row; count++)
+	{
+		for(int count2 = 0; count2 < col; count2++)
+		{
+            bool antFlag = true; //This will be used if a space is occupied;
+            //Adding the doodle bugs to the array
+            while(antFlag)
+            {
+                //If there are no more ants
+                if(ant == 0)
+                {
+                    goto end2;
+                }
+
+                //If the space can be occupied by a critter
+    			if(board[count][count2] != 'O' || board[count][count2] != 'X')
+                {
+                    antFlag = false;
+                    board[count][count2] = 'O'; //Setting the space as an ant
+
+                }
+                else //The space is occupied
+                {
+                    //I will need to call the class to get a new random location
+                }
+            }
+		}
+	} 
+
+    end2:
+    std::cout << "All of the ants have been placed" << std::endl;
+
 }
