870b41c48ce340b2da3954801b1310d4391ba7be
Zach <30414090+zachmkhan@users.noreply.github.com>
2018-05-02 11:22:20 -0700

    Update Board.hpp

--- Board.hpp
+++ Board.hpp
@@ -3,6 +3,14 @@
 
 class Board
 {
-    //
+    private:
+        char ** board; //This will be used to dynamically allocate the array
+        int row; //The number of rows (Y value)
+        int col; //The number of cols (X value)
+    public:
+        Board(); //Default constructor. Will be set to 20x20 by default
+        //Board(int row, int col); //This will possibly be used for the extra credit
+        void printBoard(); //Prints the game board
+        void deleteArray(); //Frees the dynamic memory
 };
 #endif
