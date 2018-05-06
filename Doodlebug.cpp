1e1638752600871ab64a72b400f9de39d2ce9581
ryanalcorn88 <39017022+ryanalcorn88@users.noreply.github.com>
2018-05-05 21:37:36 -0700

    Update Doodlebug.cpp

--- Doodlebug.cpp
+++ Doodlebug.cpp
@@ -2,14 +2,14 @@
 #include <ctime>					// For time()
 #include <cstdlib>					// For srand() and rand()
 
-void Doodlebug::Move(Critter *** board,int row, int col) 
+
+void Doodlebug::Move(Critter *** board,int row, int col)
 {
 	//Tries to go up to eat an ant
 	if (board[xLocation][yLocation + 1]->getCritterType() == 'O')
 	{
-		board[xLocation][yLocation] = board[xLocation][yLocation + 1];		//The Doodlebug moves to the new space
+        	board[xLocation][yLocation] = board[xLocation][yLocation + 1];		//The Doodlebug moves to the new space
 		board[xLocation][yLocation] = nullptr;					//The Doodlebug's old space becomes null
-
 		stepsSinceEating = 0;							//The counter is reset due to the eaten ant
 		stepsSinceBreeding += 1;						//Increment how long since the doodlebug had babies
 		yLocation += 1;								//The Doodlebug moves up
@@ -19,7 +19,6 @@ void Doodlebug::Move(Critter *** board,int row, int col)
 	{
 		board[xLocation][yLocation] = board[xLocation][yLocation - 1];		//The Doodlebug moves to the new space
 		board[xLocation][yLocation] = nullptr;					//The Doodlebug's old space becomes null
-
 		stepsSinceEating = 0;							//The counter is reset due to the eaten ant
 		stepsSinceBreeding += 1;						//Increment how long since the doodlebug had babies
 		yLocation -= 1;								//The Doodlebug moves down
@@ -29,7 +28,6 @@ void Doodlebug::Move(Critter *** board,int row, int col)
 	{
 		board[xLocation][yLocation] = board[xLocation + 1][yLocation];		//The Doodlebug moves to the new space
 		board[xLocation][yLocation] = nullptr;					//The Doodlebug's old space becomes null
-
 		stepsSinceEating = 0;							//The counter is reset due to the eaten ant
 		stepsSinceBreeding += 1;						//Increment how long since the doodlebug had babies
 		xLocation += 1;								//The Doodlebug moves right
@@ -39,7 +37,6 @@ void Doodlebug::Move(Critter *** board,int row, int col)
 	{
 		board[xLocation][yLocation] = board[xLocation][yLocation + 1];		//The Doodlebug moves to the new space
 		board[xLocation][yLocation] = nullptr;					//The Doodlebug's old space becomes null
-
 		stepsSinceEating = 0;							//The counter is reset due to the eaten ant
 		stepsSinceBreeding += 1;						//Increment how long since the doodlebug had babies
 		xLocation -= 1;								//The Doodlebug moves left
@@ -48,10 +45,8 @@ void Doodlebug::Move(Critter *** board,int row, int col)
 	else
 	{
 		int randomMove = rand() % 4 + 1;					//Random number for the 4 different directions
-
 		switch (randomMove)
 		{
-		
 		//Moving Up
 		case 1:
 			if (yLocation < row && board[xLocation][yLocation + 1] == nullptr)
@@ -61,56 +56,43 @@ void Doodlebug::Move(Critter *** board,int row, int col)
 
 				yLocation += 1;														//The Doodlebug moves up
 			}
-
 			break;
-		
 		//Moving Down
 		case 2:
 			if (yLocation > 0 && board[xLocation][yLocation - 1] == nullptr)
 			{
 				board[xLocation][yLocation] = board[xLocation][yLocation - 1];		//The Doodlebug moves to the new space
 				board[xLocation][yLocation] = nullptr;
-
 				yLocation -= 1;														//The Doodlebug moves up
 			}
-
 			break;
-		
 		//Moving Right
 		case 3:
 			if (xLocation < col && board[xLocation + 1][yLocation] == nullptr)
 			{
 				board[xLocation][yLocation] = board[xLocation + 1][yLocation];		//The Doodlebug moves to the new space
 				board[xLocation][yLocation] = nullptr;
-
 				xLocation += 1;														//The Doodlebug moves up
 			}
-			
 			break;
-		
 		//Moving Left
 		case 4:
 			if (xLocation > 0 && board[xLocation - 1][yLocation] == nullptr)
 			{
 				board[xLocation][yLocation] = board[xLocation - 1][yLocation];		//The Doodlebug moves to the new space
 				board[xLocation][yLocation] = nullptr;
-
 				xLocation -= 1;														//The Doodlebug moves up
 			}
-
 			break;
 		}
-
 		stepsSinceEating += 1;							//Increment how long since the doodlebug has eaten
 		stepsSinceBreeding += 1;						//Increment how long since the doodlebug had babies
-
 	}
 }
 
 /**********************************************************************************************************
 ** Description: Ant::Breed creates an ant in a random empty cell ajacent to the ant who calls the function
 **********************************************************************************************************/
-
 void Doodlebug::Breed(Critter *** board, int row, int col)
 {
 	int randomMove = rand() % 4 + 1;										//Random number for the 4 different directions
@@ -118,49 +100,61 @@ void Doodlebug::Breed(Critter *** board, int row, int col)
 
 	switch (randomMove)
 	{
-
 		//Breeding Up
 	case 1:
 		if (yLocation < row && board[xLocation][yLocation + 1] == nullptr)
 		{
 			// need to change the location to Doodlebug by changing pointer to critter, not sure how yet
 			board[xLocation][yLocation + 1]->setCritterType('O');		//Set critter type to O for new doodlebug
-
 		}
-
 		break;
-
 		//Breeding Down
 	case 2:
 		if (yLocation > 0 && board[xLocation][yLocation - 1] == nullptr)
 		{
 			// need to change the location to Doodlebug by changing pointer to critter, not sure how yet
+
 			board[xLocation][yLocation - 1]->setCritterType('O');		//Set critter type to O for new doodlebug
-
 		}
-
 		break;
-
 		//Breeding Right
 	case 3:
+
 		if (xLocation < col && board[xLocation + 1][yLocation] == nullptr)
 		{
 			// need to change the location to Doodle bug by changing pointer to critter, not sure how yet
 			board[xLocation + 1][yLocation]->setCritterType('O');		//Set critter type to O for new doodlebug
-
 		}
-
 		break;
-
 		//Breeding Left
 	case 4:
+
 		if (xLocation > 0 && board[xLocation - 1][yLocation] == nullptr)
 		{
 			// need to change the location to Doodlebug by changing pointer to critter, not sure how yet
 			board[xLocation - 1][yLocation]->setCritterType('O');	//Set critter type to O for new doodlebug
-
 		}
-
 		break;
 	}
+}
 
+/**********************************************************************************************************
+** Description: Function runs through the board objects and deletes any Doodlebugs whose StepsSinceEating
+** is equal to 3.
+**********************************************************************************************************/
+void Doodlebug::Starve(Critter*** board)
+{
+    int arraySize = sizeof(board) / sizeof(*board);
+    Critter* ptr;
+    for(int x = 0; x < arraySize; x++)
+    {
+        ptr = *board[x];
+
+        if(ptr->getCritterType() == 'X' && ptr->getStepsSinceEating() >= 3)
+        {
+            board[x] = nullptr;
+        }
+    }
+
+    delete ptr;
+}
