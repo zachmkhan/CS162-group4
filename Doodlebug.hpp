895c11a2bb5f81ca45901abd4d5f22695ec6a8a9
ryanalcorn88 <39017022+ryanalcorn88@users.noreply.github.com>
2018-05-05 21:38:14 -0700

    Update Doodlebug.hpp

--- Doodlebug.hpp
+++ Doodlebug.hpp
@@ -1,19 +1,22 @@
-#ifndef DOODLEBUG_HPP
-#define DOODLEBUG_HPP
+#ifndef DOODLEBUG_HPP_INCLUDED
+#define DOODLEBUG_HPP_INCLUDED
 #include "Critter.hpp"
 
 class Doodlebug : public Critter
 {
-    private:
-        int stepsSinceBreeding; //steps since breeding. Will set to 0 in the constructor.
-		int stepsSinceEating; //steps since eating
-        int stepsNeededToBreed = 8; //Doodlebug will breeed every 8 steps
-        char gameBoardCharacter = 'X'; //The doodlebug will have this space in the game
-    public:
-        Doodlebug(); //Empty constructor
-        virtual void Move(Critter *** board, int row, int col); //Virtual function
-		void Breed(Critter *** board, int row, int col);       //When the ant breeds
-        
-        //char getCharacter(); //Gets the character for the board
+private:
+    int stepsSinceBreeding; //steps since breeding. Will set to 0 in the constructor.
+    int stepsSinceEating; //steps since eating
+    int stepsNeededToBreed = 8; //Doodlebug will breeed every 8 steps
+    char gameBoardCharacter = 'X'; //The doodlebug will have this space in the game
+
+public:
+    Doodlebug(); //Empty constructor
+    virtual void Move(Critter *** board, int row, int col); //Virtual function
+    void Breed(Critter *** board, int row, int col);       //When the ant breeds
+    void Starve(Critter*** board);
+    //char getCharacter(); //Gets the character for the board
+
 };
-#endif
+
+#endif // DOODLEBUG_HPP_INCLUDED
