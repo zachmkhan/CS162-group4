9e16647c7094c498b2e8a0415d001544ffb75439
ryanalcorn88 <39017022+ryanalcorn88@users.noreply.github.com>
2018-05-05 21:38:37 -0700

    Update Critter.hpp

--- Critter.hpp
+++ Critter.hpp
@@ -1,15 +1,18 @@
-#ifndef CRITTER_HPP
-#define CRITTER_HPP
+#ifndef CRITTER_HPP_INCLUDED
+#define CRITTER_HPP_INCLUDED
 
 class Critter
 {
     protected:
         int xLocation;
         int yLocation;
+        int stepsSinceEating;
         char gameBoardCharacter;
     public:
 		char getCritterType();
+		int getStepsSinceEating();
 		void setCritterType(char);
         virtual void Move() = 0;
 };
-#endif
+
+#endif // CRITTER_HPP_INCLUDED
